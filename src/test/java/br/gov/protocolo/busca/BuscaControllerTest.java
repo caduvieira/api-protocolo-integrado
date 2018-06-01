9513b9a0251c67ebe3deb6f504b51ce5b062b5d7
Carlos Vieira <edu.carlos.vieira@gmail.com>
2018-06-01 10:02:02 -0300

    Altera do mockito para MockMvc

--- src/test/java/br/gov/protocolo/busca/BuscaControllerTest.java
+++ src/test/java/br/gov/protocolo/busca/BuscaControllerTest.java
@@ -2,42 +2,48 @@ package br.gov.protocolo.busca;
 
 import br.gov.protocolo.busca.BuscaController;
 import br.gov.protocolo.busca.BuscadorConteudo;
-import br.gov.protocolo.model.Resultado;
 import lombok.experimental.FieldDefaults;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mock;
-import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
 
 
 import static br.gov.protocolo.fixtures.TestData.RESULTADO;
 import static lombok.AccessLevel.PRIVATE;
 import static java.util.Optional.of;
 import static org.mockito.Mockito.when;
-import static org.junit.Assert.assertEquals;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
-@RunWith(MockitoJUnitRunner.class)
 @FieldDefaults(level = PRIVATE)
+@RunWith(SpringRunner.class)
+@WebMvcTest(BuscaController.class)
+@AutoConfigureMockMvc
 public class BuscaControllerTest {
 
-    @Mock
+    @MockBean
     BuscadorConteudo buscador;
 
-    BuscaController controller;
-
-    @Before
-    public void setUp() {
-        controller = new BuscaController(buscador);
-    }
+    @Autowired
+    private MockMvc mockMvc;
 
     @Test
     public void retornaBusca() throws Exception {
         when(buscador.busca(of("teste"),0)).thenReturn(RESULTADO);
 
-        Resultado resultado = controller.busca("teste",1);
-        assertEquals(resultado,RESULTADO);
+        this.mockMvc.perform(get("/busca").param("q", "teste")).andDo(print())
+          .andExpect(status().isOk())
+          .andExpect(jsonPath("$.totalResultados").value("2"))
+          .andExpect(jsonPath("$.totalPaginas").value("1"))
+          .andExpect(jsonPath("$.paginaAtual").value("1"));
     }
 
 }
