58273d0f01afc2a643b6255ff9ac0afd0f6bae89
Carlos Vieira <edu.carlos.vieira@gmail.com>
2018-06-04 08:46:08 -0300

    Adiciona CrossOrigin annotation para habilitar CORS

--- src/main/java/br/gov/protocolo/busca/BuscaController.java
+++ src/main/java/br/gov/protocolo/busca/BuscaController.java
@@ -5,6 +5,7 @@ import lombok.experimental.FieldDefaults;
 import net.rossillo.spring.web.mvc.CacheControl;
 import net.rossillo.spring.web.mvc.CachePolicy;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,6 +27,7 @@ class BuscaController {
     }
 
     @CacheControl(maxAge=31556926, policy = { CachePolicy.MUST_REVALIDATE })
+    @CrossOrigin
     @RequestMapping("/busca")
     public Resultado busca(@RequestParam(required = false) String q,
                                  @RequestParam(required = false, defaultValue = "1") Integer pagina) {
